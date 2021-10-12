20f473908aee39ddf6142bb03780150aefe0a73e
jfkeci <ja.lip132@gmail.com>
2021-10-12 12:03:38 +0200

    feat: Header component

--- src/main.js
+++ src/main.js
@@ -2,7 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
 Vue.config.productionTip = false
+
+Vue.prototype.$appName = 'My App'
 
 new Vue({
   router,
